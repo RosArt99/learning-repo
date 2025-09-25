2f6d350a86bac5bce8780009d1bd29fceb24068e
Ros <kukishvmasle@gmail.com>
2025-09-25 18:55:49 +0200

    FINAL

--- homework_module_3_task_2.py
+++ homework_module_3_task_2.py
@@ -4,25 +4,25 @@ def get_numbers_ticket():
     while True: #turning on looper
         try:
             min = int(input("Enter minimum number: "))
-            if min < 1:
+            if min < 1: #minimum number can't be less than 1
                 print(f"Minimum number can\'t be negative or equal to zero, you entered:{min}")
                 continue
-            if min > 999:
+            if min > 999: # minimum number can't be more than 999
                 print(f"Minimum number can\'t be higher than 999, you entered:{min}")
                 continue
             max = int(input("Enter maximum number: "))
-            if max > 1000:
+            if max > 1000: #maximum number can't be more than 1000
                 print(f"Maximum number can\'t be higher than 1000, you entered:{max}")
                 continue
-            if max < 1:
+            if max < 1: # maximum number can't be less than 1
                 print(f"Maximum number can\'t negative or equal to zero, you entered:{max}")
                 continue
             quantity = int(input("Enter quantity between min and max: "))         
-            if quantity > (max - min+1):
+            if quantity > (max - min+1): # quantity can't be less than selected range
                 print(f"Quantity is too large, reduce quantity for provided range min:{min}, max{max}")
                 continue
-            random_list = random.sample(range(min, max+1), quantity)
-            random_list.sort()
+            random_list = random.sample(range(min, max+1), quantity) 
+            random_list.sort() 
             return random_list
         except ValueError:
             print("Enter integers only!")
