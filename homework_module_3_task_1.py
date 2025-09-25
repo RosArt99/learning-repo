25e13d2ab684efc7e76f90056e6fbe2de88e3cf4
Ros <kukishvmasle@gmail.com>
2025-09-25 19:02:52 +0200

    FINAL

--- homework_module_3_task_1.py
+++ homework_module_3_task_1.py
@@ -3,7 +3,7 @@ from datetime import datetime
 def get_days_from_today(): #date was removed from function arguments. Incorporated in function instead.
     while True: #turning on loop
         try: 
-            date = str(input("Enter the date in format \"YYYY-MM-DD\": ")) #\ to ignore "
+            date = str(input("Enter the date in format \"YYYY-MM-DD\": ")) #\ to ignore """
             converted_time = datetime.strptime(date, "%Y-%m-%d").date() #place where ValueError occurs 
         except ValueError:
             print("Invalid time format, try again, please, use \"YYYY-MM-DD\" format.")
